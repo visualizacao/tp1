24ea58ad2796c25c9ba17282b10d0fef7aaf2b20
root <root@skolldir.localdomain>
2015-04-10 21:54:50 -0300

    index arrumado

--- index.php
+++ index.php
@@ -172,6 +172,13 @@
 
 
     <?php echo "readFile.php";?>
-
+    <?php
+      $input = fopen("http://homepages.dcc.ufmg.br/~dhca/all.txt", "r") or die("Unable to open file!");
+      // Output one line until end-of-file
+      while(!feof($input)) {
+        echo (fgets($input) . "<br>");
+      }
+      fclose($input);
+    ?>
   </body>
 </html>
